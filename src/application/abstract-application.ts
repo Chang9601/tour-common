56dc322c4aaf09f372ac5116ca2b83e8cc66e12c
Chang9601 <changsup96@gmail.com>
2024-05-24 14:48:29 +0900

    공통 모듈 갱신

--- src/application/abstract-application.ts
+++ src/application/abstract-application.ts
@@ -47,13 +47,13 @@ export abstract class AbstractApplication {
      * 4.16.0 부터 body-parser 모듈의 일부 기능이 express 모듈에 내장되어 따로 설치할 필요는 없지만 버퍼나 텍스트 형식의 데이터 처리시엔 따로 설치해서 사용해서 한다.
      * body-parser 모듈은 JSON, URL-encoded 형식 외 Raw, Text 형식의 데이터도 추가로 해석할 수 있는데 Raw는 요청의 본문이 버퍼 데이터인 경우 Text는 텍스트 데이터일 경우이다.
      */
-    this.app.use(bodyParser.json());
+    // this.app.use(bodyParser.json());
     if (process.env.NODE_ENV === 'development') {
       /*
        * morgan 모듈은 요청과 응답에 대한 정보를 콘솔에 기록하는 미들웨어이다.
        * 개발 환경에서는 dev, 배포 환경에서는 combined 를 사용한다.
        */
-      this.app.use(morgan('dev'));
+      //  this.app.use(morgan('dev'));
     }
   }
 
--- src/type/query-request.ts
+++ src/type/query-request.ts
@@ -4,5 +4,5 @@ export type RequsetParam = {};
 export type RequestBody = {};
 export type RequestQuery = { [key: string]: string };
 export type ResponseBody = {};
-// TODO: why?
+// TODO: <>에 타입을 사용하면 오류 발생하지만 {} 리터럴 사용 시 오류 없음.
 export type QueryRequest = Request<{}, {}, {}, {}>;
