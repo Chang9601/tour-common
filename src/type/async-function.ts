e6c4cd518681a693f817b33fc1252e8a28d55be0
changsup <changsup96@gmail.com>
2024-06-04 20:07:16 +0900

    공통 모듈 갱신

--- src/type/async-function.ts
+++ src/type/async-function.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 
 import { QueryRequest } from './query-request';
+import { RequestWithUser } from './auth-type';
 
 export type AsyncFunction = (
-  request: Request | QueryRequest,
+  request: Request | QueryRequest | RequestWithUser,
   response: Response,
   next: NextFunction
 ) => Promise<void>;
