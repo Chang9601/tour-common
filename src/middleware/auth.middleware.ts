4536a6368ccde0c15bd626cf2021ff69c1dbc9c8
changsup <changsup96@gmail.com>
2024-09-03 22:26:44 +0900

    공통 모듈 갱신

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -59,7 +59,7 @@ export const authenticationMiddleware = catchAsync(
 
       try {
         currentUser = await axios.get(
-          `http://tour:3000/api/v1/users/current-user`,
+          `http://auth:3000/api/v1/users/current-user`,
           { params: { id: decoded.id } }
         );
 
